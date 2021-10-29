0d222ed2520801bae28d7b8842b0ad6c9c8f523a
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-10-29 23:03:48 +0400

    feat: add Object.defineproperties

---
+++ stage_2/interview/JavaScript/Objects_Built-in_methods/Know Static Object Methods/object.defineproperties.js
@@ -0,0 +1,13 @@
+// it is like a object.defineproperties but it adds several properies to an object
+const object1 = {};
+
+Object.defineProperties(object1, {
+  property1: {
+    value: 42,
+    writable: true
+  },
+  property2: {}
+});
+
+console.log(object1.property1);
+// expected output: 42
