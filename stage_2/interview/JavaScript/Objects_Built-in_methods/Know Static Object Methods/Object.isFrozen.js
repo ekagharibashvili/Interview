44fc4aa57253a3647e2db2e45dc161a220ce36bf
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-11-02 22:32:51 +0400

    feat: add isfrozen

---
+++ stage_2/interview/JavaScript/Objects_Built-in_methods/Know Static Object Methods/Object.isFrozen.js
@@ -0,0 +1,13 @@
+// determines if an object is frozen.
+const object1 = {
+    property1: 42
+};
+
+console.log(Object.isFrozen(object1));
+// expected output: false
+
+Object.freeze(object1);
+
+console.log(Object.isFrozen(object1));
+  // expected output: true
+
