66a138c9e375cc307fe6d874fe8aee85b46b7da3
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-11-02 22:35:42 +0400

    feat: add object.isExtensible

---
+++ stage_2/interview/JavaScript/Objects_Built-in_methods/Know Static Object Methods/Object.isExtensible.js
@@ -0,0 +1,10 @@
+// determines id an object is extensible, whether it can have new properties added to it
+const object1 = {};
+
+console.log(Object.isExtensible(object1));
+// expected output: true
+
+Object.preventExtensions(object1);
+
+console.log(Object.isExtensible(object1));
+// expected output: false
