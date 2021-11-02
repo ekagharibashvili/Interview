ccc3163050baccaf5c9c84ecbcf9cbd1f2cfdd88
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-11-02 22:48:07 +0400

    feat: add getOwnpropertiesNAmes

---
+++ stage_2/interview/JavaScript/Objects_Built-in_methods/Know Static Object Methods/Object.getOwnpropertiesNames.js
@@ -0,0 +1,9 @@
+// it return property names Array includin non-enumarable properties
+const object1 = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+
+console.log(Object.getOwnPropertyNames(object1));
+  // expected output: Array ["a", "b", "c"]
