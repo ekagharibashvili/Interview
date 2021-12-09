4136309d3ef62feedf865d263c666425e8aa73b1
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-12-09 16:59:58 +0400

    feat: add difference between types and interfaces

---
+++ stage_2/interview/TypeScript/types and interface differences.ts
@@ -0,0 +1,18 @@
+// Type
+// It is a collection of data types.
+// It supports the creation of a new name for a type.
+// It has less capabilities.
+// It does note suppport the use of an object
+// Multiple merged declarations cannot be used.
+// Two types having the same name raise an exception.
+// It does not have implementation purpoeses.
+
+
+// Interface 
+// It is a form of syntax.
+// It provides a way to define entities.
+// It has more capabilities.
+// It supports the use of an object.
+// Multiple merged declarations can be used.
+// Two interfaces having the same name get merged.
+// It has implementation purpose.
